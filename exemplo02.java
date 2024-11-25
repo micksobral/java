048680a64aa20935627985833b6e04949e6b79e5
mickaelle sobral <mickaellesobral@gmail.com>
2024-11-25 09:33:01 -0300

    Add files via upload

---
+++ exemplo02.java
@@ -0,0 +1,15 @@
+package fundaamentos02;
+
+public class exemplo02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int idade = 15;
+		while (idade<18){
+		System.out.println(idade);
+		idade = idade+1;
+		}
+	}
+
+}
